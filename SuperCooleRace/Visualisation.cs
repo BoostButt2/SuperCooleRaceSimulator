9d337b0a52c19316e7eb46210c992974e8aab1af
Jesse Huang <huangjesse3@gmail.com>
2020-10-01 21:26:09 +0200

    I am a dumbass

--- SuperCooleRace/Visualisation.cs
+++ SuperCooleRace/Visualisation.cs
@@ -7,7 +7,8 @@ namespace SuperCooleRace
 {
     public static class Visualisation
     {
-
+        public static IParticipant driverLeft;
+        public static IParticipant driverRightt;
 
         public static void Initialize()
         {
@@ -15,16 +16,16 @@ namespace SuperCooleRace
         }
 
         #region graphics
-        private static string[] _finishHorizontal = { "----", " # ", " # ", "----" };
-        private static string[] _finishVertical = { "|  |", "|##|", "|  |", "|  |" };
-        private static string[] _startHorizontal = { "----", " 8 ", " 8 ", "----" };
-        private static string[] _startVertical = { "|  |", "|88|", "|  |", "|  |" };
+        private static string[] _finishHorizontal = { "----", " <# ", " ># ", "----" };
+        private static string[] _finishVertical = { "|  |", "|##|", "|<>|", "|  |" };
+        private static string[] _startHorizontal = { "----", " <8 ", " >8 ", "----" };
+        private static string[] _startVertical = { "|  |", "|88|", "|<>|", "|  |" };
         private static string[] _straightHorizontal = {"----", "    ", "    ", "----" };
-        private static string[] _straightVertical = {"|  |", "|  |", "|  |", "|  |" };
-        private static string[] _rightCorner = {"----", "   |", "   |", "-  |" };
-        private static string[] _superRightCorner = {"|  -", "|   ", "|   ", "----" };
-        private static string[] _leftCorner = {"-  |", "   |", "   |", "----" };
-        private static string[] _superLeftCorner = {"----", "|   ", "|   ", "|  -" };
+        private static string[] _straightVertical = {"|  |", "|  |", "|<>|", "|  |" };
+        private static string[] _rightCorner = {"----", "   |", " <>|", "-  |" };
+        private static string[] _superRightCorner = {"|  -", "|   ", "|<> ", "----" };
+        private static string[] _leftCorner = {"-  |", "   |", " <>|", "----" };
+        private static string[] _superLeftCorner = {"----", "|   ", "|<> ", "|  -" };
 
 
         #endregion
@@ -37,73 +38,90 @@ namespace SuperCooleRace
                 {
                     foreach (string finish in _finishHorizontal)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.FinishVertical)
                 {
                     foreach (string finish in _finishVertical)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.StartGrid)
                 {
                     foreach (string finish in _startHorizontal)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.StartVertical)
                 {
                     foreach (string finish in _startVertical)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.Straight)
                 {
                     foreach (string finish in _straightHorizontal)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.StraightVertical)
                 {
                     foreach (string finish in _straightVertical)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.RightCorner)
                 {
                     foreach (string finish in _rightCorner)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.SuperRightCorner)
                 {
                     foreach (string finish in _superRightCorner)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.LeftCorner)
                 {
                     foreach (string finish in _leftCorner)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
                 if (sect.SectionType == SectionTypes.SuperLeftCorner)
                 {
                     foreach (string finish in _superLeftCorner)
                     {
-                        Console.WriteLine(finish);
+                        string sectionMetDriver = PlaceDriver(finish, driverLeft, driverRightt);
+                        Console.WriteLine(sectionMetDriver);
                     }
                 }
             }
+        }
+
+        public static string PlaceDriver(string s, IParticipant leftDriver, IParticipant rightDriver)
+        {
+            s = s.Replace("<", "L");
+            s = s.Replace(">", "R");
+            return s;
         }
 
     }
