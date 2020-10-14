1b4c3174dbde7624d5d962158892b035cbe87a1b
Jesse Huang <huangjesse3@gmail.com>
2020-10-14 19:28:05 +0200

    Begonnen met 6

---
+++ Controller/RaceData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controller
+{
+    class RaceData<T>
+    {
+
+        private List<T> _list = new List<T>();
+
+        public void AddList(T value)
+        {
+            _list.Add(value);
+        }
+    }
+}
