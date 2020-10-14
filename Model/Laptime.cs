511653773af20fc70716cb0d5754f9b868b1eb77
Jesse Huang <huangjesse3@gmail.com>
2020-10-14 19:38:41 +0200

    Peepee poopoo

---
+++ Model/Laptime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Laptime
+    {
+        public string name { get; set; }
+        public TimeSpan time { get; set; }
+
+
+    }
+}
---
+++ Model/Results.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Results
+    {
+        public string name { get; set; }
+        public int points { get; set; }
+    }
+}
